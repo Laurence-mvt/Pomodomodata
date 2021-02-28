aff3e68eee2ed7c3063a02e020800e2be2ffaff1
Laurence-mvt <laurencefinch@brandeis.edu>
2021-02-28 01:44:33 -0500

    improved UI with formatting

--- pomo.py
+++ pomo.py
@@ -16,15 +16,25 @@ if len(sys.argv) > 1:
     if sys.argv[1].isdecimal():
         pomsToLongBreak = int(sys.argv[1])
 
-# if run in test mode, set focus time as 2 seconds, break time as 5 seconds
-if sys.argv[1] == 't' or sys.argv[2] == 't':
-    focusTime = 0
-    normalBreakTime = 0
-    longBreakTime = 1
-    testFocusTime = 2
-    testNormalBreakTime = 5
-    testLongBreakTime = 10
+testMode = False
+if len(sys.argv) > 2:
+    if sys.argv[2] == 't':
+        testMode = True
 
+# if run in test mode, set focus time as 2 seconds, break time as 5 seconds
+if len(sys.argv) > 1:
+    if sys.argv[1] == 't' or testMode:
+        focusTime = 0
+        normalBreakTime = 0
+        longBreakTime = 0
+        testFocusTime = 2
+        testNormalBreakTime = 5
+        testLongBreakTime = 10
+
+# get notification sound
+sound = os.listdir('useEndSound')[0]
+soundPath = str(Path('useEndSound')/sound)
+wave_obj = simpleaudio.WaveObject.from_wave_file(soundPath)
 
 # if the first time running the program, create a new csv with headers to store data
 p = Path('myPomoData.csv')
@@ -49,93 +59,112 @@ dataBuffer = []
 #               'mood': 1-5 rating of how cheerful/happy/content user was (5 highest),
 #               'comment': what user did, how they felt, any observation}
 
+# start up display
+print(f'\n')
+print('  PomodomoData  '.center(85, '_'))
+print(f'\n')
+print(f'Ciao, world! Welcome to PomodomoData, a Pomodoro app.\n'.ljust(85, ' '))
+print('------'.center(85, ' ') + '\n')
+print(f'PomodomoData allows you to track your productivity, focus, mood and tiredness levels.\n'.ljust(85))
+print(f'''After each focus session enter: q to quit or s to skip break (optional)
+... ### for focus, tired and mood levels (1-5)...
+... and a comment on your pom session (optional).
+
+For example, "554 completed report plan" or, "q 353 time for bed".
+
+Press ctrl-C to save your progress and quit the app at any time.\n''')
+print('------'.center(85, ' ') + '\n')
+
+# -------------------POMODOROS START HERE ON USER INPUT----------------
 # start pomodoros on user input
-input('Ciao, world! Welcome to Pomodomodata, a pomodoro app. Press enter to begin first pomodoro.')
-counting = True
-# get notification sound
-sound = os.listdir('useEndSound')[0]
-soundPath = str(Path('useEndSound')/sound)
-wave_obj = simpleaudio.WaveObject.from_wave_file(soundPath)
+input('Press enter to begin first pomodoro.')
+try:
+    while True:
+        currentPom += 1
+        pomStartTime = datetime.datetime.now()
+        currentPomData={'pomSession':currentPom}
+        currentPomData['pomStartDatetime'] = pomStartTime.strftime('%Y-%m-%d %H:%M:%S')
+        print(f"Pom start time: {pomStartTime.strftime('%a, %d %b %Y: %H:%M:%S')}  ".ljust(45, '.') +  f"  Focus until: {(pomStartTime + datetime.timedelta(minutes=focusTime, seconds=testFocusTime)).strftime('%H:%M:%S')}".rjust(40, '.'))
+        print(f'Current pom: {currentPom}\n')
+        
+        # set break time depending on if long break or regular
+        if currentPom % pomsToLongBreak == 0:
+            breakTime = longBreakTime
+            testBreakTime = testLongBreakTime
+        else:
+            breakTime = normalBreakTime
+            testBreakTime = testNormalBreakTime
 
-while counting == True:
-    currentPom += 1
-    pomStartTime = datetime.datetime.now()
-    currentPomData={'pomSession':currentPom}
-    currentPomData['pomStartDatetime'] = pomStartTime
-    print(f"Pom start time: {pomStartTime.strftime('%a, %d %b %Y: %H:%M:%S')}")
-    print(f'Current pom: {currentPom}')
-    
-    # set break time depending on if long break or regular
-    if currentPom % pomsToLongBreak == 0:
-        breakTime = longBreakTime
-        testBreakTime = testLongBreakTime
-    else:
-        breakTime = normalBreakTime
-        testBreakTime = testNormalBreakTime
-
-    # after 25 minutes
-    while datetime.datetime.now() - pomStartTime < datetime.timedelta(minutes=focusTime, seconds=testFocusTime):
-        time.sleep(1)
-    # pom session complete, play notification sound
-    wave_obj.play()
-    pomsComplete += 1
-
-    pomEndTime = datetime.datetime.now()
-    currentPomData['pomEndDatetime'] = pomEndTime
-    print(f"Pom #{pomsComplete} complete - {pomEndTime.strftime('%a, %d %b %Y: %H:%M:%S')} - {breakTime} minute break until {(pomEndTime + datetime.timedelta(minutes=breakTime, seconds=testBreakTime)).strftime('%H:%M:%S')}")
-    # get user reflection on session and choice to continue or quit
-    userInput = ''
-    while len(userInput.split(' ')) < 1 or userInput == '': 
-        print('enter q to quit or s to skip break (optional) and ### for focus, tired and mood levels (1-5) and comment on your pom session (optional), eg. "554 completed report plan"')
-        userInput = input('[q,s] ### [comment]:  ')
-
-    # for hard quit with no session ratings, enter qh
-    if userInput[:2] == 'qh':
-        break
-    
-    # split user input into q/s ### and comment
-    userInput = userInput.split(' ')
-    userComment = ''
-    sessionRating = ''
-    if len(userInput) > 1: # user likely entered a rating
-        if userInput[0] in ['q', 's', 'qh']:
-            sessionRating = userInput[1]
-            if len(userInput) > 2: # user entered a comment
-                userComment = ' '.join(userInput[2:])
-        else: # rating should be first string in input
-            sessionRating = userInput[0]
-            userComment = ' '.join(userInput[1:])
-    else:
-        sessionRating = userInput[0]
-    # get session rating if not provided or not valid
-    while len(sessionRating) != 3 or not sessionRating.isdecimal() or not all(x in ['1', '2', '3', '4', '5'] for x in sessionRating):
-        sessionRatingOptionalComment = input('please record focus, tired and mood levels (1-5) and optional comment: ')
-        sessionRating = sessionRatingOptionalComment[:3]
-        if len(sessionRatingOptionalComment) > 3:
-            userComment =  sessionRatingOptionalComment[3:]
-    
-    # addd session Rating to pom session data
-    currentPomData.update({'focus': sessionRating[0], 'tired': sessionRating[1], 'mood': sessionRating[2]})
-    currentPomData['comment'] = userComment
-
-    # add current pom data to data buffer
-    dataBuffer.append(currentPomData)
-
-    # if user wants to quit, end the pomodoro loop
-    if userInput[0] == 'q':
-        break
-
-    # if user wants to skip break, start next pomodoro
-    if userInput[0] == 's':
-        continue    
-    
-    # otherwise, check if break time over:
-    if datetime.datetime.now() - pomEndTime < datetime.timedelta(minutes=breakTime, seconds=testBreakTime):
-        # break time not over
-        while datetime.datetime.now() - pomEndTime < datetime.timedelta(minutes=breakTime, seconds=testBreakTime):
+        # after 25 minutes
+        while datetime.datetime.now() - pomStartTime < datetime.timedelta(minutes=focusTime, seconds=testFocusTime):
             time.sleep(1)
-    # else break time over, play notification sound and start next pomm session
-    wave_obj.play()
+        # pom session complete, play notification sound
+        wave_obj.play()
+        pomsComplete += 1
+
+        pomEndTime = datetime.datetime.now()
+        currentPomData['pomEndDatetime'] = pomEndTime.strftime('%Y-%m-%d %H:%M:%S')
+        print(f"Pom #{pomsComplete} complete - {pomEndTime.strftime('%a, %d %b %Y: %H:%M:%S')} - {breakTime} minute break until {(pomEndTime + datetime.timedelta(minutes=breakTime, seconds=testBreakTime)).strftime('%H:%M:%S')}")
+        # get user reflection on session and choice to continue or quit
+        userInput = ''
+        while len(userInput.split(' ')) < 1 or userInput == '': 
+            userInput = input('Enter [q,s] ### [comment]:  ')
+
+        # for hard quit with no session ratings, enter qh
+        if userInput[:2] == 'qh':
+            break
+        
+        # split user input into q/s ### and comment
+        userInput = userInput.split(' ')
+        userComment = ''
+        sessionRating = ''
+        if len(userInput) > 1: # user likely entered a rating
+            if userInput[0] in ['q', 's', 'qh']:
+                sessionRating = userInput[1]
+                if len(userInput) > 2: # user entered a comment
+                    userComment = ' '.join(userInput[2:])
+            else: # rating should be first string in input
+                sessionRating = userInput[0]
+                userComment = ' '.join(userInput[1:])
+        else:
+            sessionRating = userInput[0]
+        # get session rating if not provided or not valid
+        while len(sessionRating) != 3 or not sessionRating.isdecimal() or not all(x in ['1', '2', '3', '4', '5'] for x in sessionRating):
+            sessionRatingOptionalComment = input('please record focus, tired and mood levels (1-5) and optional comment: ')
+            sessionRating = sessionRatingOptionalComment[:3]
+            if len(sessionRatingOptionalComment) > 3:
+                userComment =  sessionRatingOptionalComment[3:]
+        
+        # addd session Rating to pom session data
+        currentPomData.update({'focus': sessionRating[0], 'tired': sessionRating[1], 'mood': sessionRating[2]})
+        currentPomData['comment'] = userComment
+
+        # add current pom data to data buffer
+        dataBuffer.append(currentPomData)
+
+        # if user wants to quit, end the pomodoro loop
+        if userInput[0] == 'q':
+            break
+
+        # if user wants to skip break, start next pomodoro
+        if userInput[0] == 's':
+            continue    
+        
+        # otherwise, check if break time over:
+        if datetime.datetime.now() - pomEndTime < datetime.timedelta(minutes=breakTime, seconds=testBreakTime):
+            # break time not over
+            while datetime.datetime.now() - pomEndTime < datetime.timedelta(minutes=breakTime, seconds=testBreakTime):
+                time.sleep(1)
+        # else break time over, play notification sound and start next pomm session
+        wave_obj.play()
+
+# if user interupts to quit, save data before quitting
+except KeyboardInterrupt:
+    with open('myPomoData.csv', 'a') as pomDataCSV:
+        pomDictWriter = csv.DictWriter(pomDataCSV, fieldnames=['pomSession','pomStartDatetime', 'pomEndDatetime', 'focus', 'tired', 'mood', 'comment'])
+        for pom in dataBuffer:
+            pomDictWriter.writerow(pom)
+    sys.exit(f"\nGood job! See you soon.")
 
 # save pom sessions data 
 with open('myPomoData.csv', 'a') as pomDataCSV:
@@ -144,4 +173,4 @@ with open('myPomoData.csv', 'a') as pomDataCSV:
         pomDictWriter.writerow(pom)
 
 
-print('END OF PROGRAM')
+print('Good job! See you soon.')
